3d32983102f8b1a5eb9e6dd443f17a47eeec2b26
Steffen Carlsen <SteffenCarlsen@taffe.com>
2025-04-21 00:48:48 +0200

    Attempt at fixing some W11 specific issues

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using System;
+using System.IO;
 
 namespace JagexAccountSwitcher;
 
@@ -9,8 +10,18 @@ class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        try
+        {
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
+        catch (Exception ex)
+        {
+            File.WriteAllText("error.log", ex.ToString());
+            throw;
+        }
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
